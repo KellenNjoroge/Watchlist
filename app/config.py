f719690e6dfe764c1a27a298add4e3e4cc431ad8
KellenNjoroge <muthonkel@gmail.com>
2018-08-27 18:08:43 +0300

    movie.htl

---
+++ app/config.py
@@ -0,0 +1,26 @@
+class Config:
+    '''
+    General configuration
+
+    '''
+    pass
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class 
+
+    
+    Args:
+    Config: The parent configuration class with general configuration settings
+
+    '''
+    pass
+class DevConfig(Config):
+    '''
+    Development configuration child class
+
+    Args:
+        Config:The parent configuration class with Genera configuration settings
+
+    '''
+    DEBUG = True
