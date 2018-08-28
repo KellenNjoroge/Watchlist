da72483a77f92b511375994923d12ab10f6a7ddf
KellenNjoroge <muthonkel@gmail.com>
2018-08-28 10:43:57 +0300

    always commit

--- app/config.py
+++ app/config.py
@@ -1,26 +1,29 @@
 class Config:
-    '''
+    """
     General configuration
 
-    '''
+    """
     pass
 
-class ProdConfig(Config):
-    '''
-    Production configuration child class 
 
-    
+class ProdConfig(Config):
+    """
+    Production configuration child class
+
+
     Args:
     Config: The parent configuration class with general configuration settings
 
-    '''
+    """
     pass
+
+
 class DevConfig(Config):
-    '''
+    """
     Development configuration child class
 
     Args:
-        Config:The parent configuration class with Genera configuration settings
+    Config:The parent configuration class with Genera configuration settings
 
-    '''
+    """
     DEBUG = True
