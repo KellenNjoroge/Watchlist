321bb4ce1b316de702a14f6a3ba4f30a1e11130a
KellenNjoroge <muthonkel@gmail.com>
2018-09-03 16:35:42 +0300

    virtual db

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 
 def create_app(config_name):
@@ -14,7 +16,7 @@ def create_app(config_name):
     config_options[config_name].init_app(app)
     # Initializing flask extensions
     bootstrap.init_app(app)
-
+    db.init_app(app)
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
@@ -27,12 +29,3 @@ def create_app(config_name):
 
     return app
 
-# Setting up configuration
-# app.config.from_object(DevConfig)
-# app.config.from_pyfile('config.py')
-
-# Initializing Flask Extensions
-# bootstrap = Bootstrap(app)
-
-# from app import views
-# from app  import error
